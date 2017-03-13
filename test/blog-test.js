f2a0163cca1494894c1fa504ede5b833ab7395ee
Diky Arga <dikyarga.id@gmail.com>
2017-03-13 16:29:21 +0700

    read all post data passed

--- test/blog-test.js
+++ test/blog-test.js
@@ -22,20 +22,21 @@ describe('Server is running test', () => {
 })
 
 describe('CRUD Post test', () => {
+  describe('Read all data', () => {
     it('Should be return array of post when try to access "/"', (done) => {
-        chai.request('http://localhost:3000').get('/posts').end((err, res) => {
-            if (err) {
-                console.log(err);
-                done()
-            } else {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('array');
-                res.body[0].should.have.property('title');
-                res.body[0].should.have.property('body');
-                // res.body[0].should.have.property('slug');
-                done()
-            }
-        });
+      chai.request('http://localhost:3000').get('/posts').end((err, res) => {
+        if (err) {
+          done()
+        } else {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body[0].should.have.property('title');
+          res.body[0].should.have.property('body');
+          res.body[0].should.have.property('slug');
+          done()
+        }
+      });
     })
+  })
 })
