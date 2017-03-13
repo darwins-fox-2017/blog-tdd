a4b9727453a1d9d34d5c7a1a3ea4b8eaa30794ca
Diky Arga <dikyarga.id@gmail.com>
2017-03-13 15:06:49 +0700

    let's code as test expected

--- test/blog-test.js
+++ test/blog-test.js
@@ -7,10 +7,35 @@ chai.use(chaiHttp)
 
 
 describe('Server is running test', () => {
-  it('Should be return "pong" when try to access /ping', (done) => {
-    chai.request('http://localhost:3000').get('/').end((err, res) => {
-      res.should.be.json();
-      done()
-    });
-  })
+    it('Should be return "pong" when try to access /ping', (done) => {
+        chai.request('http://localhost:3000').get('/ping').end((err, res) => {
+            if (err) {
+                console.log(err);
+                done()
+            } else {
+                // res.body.should.be.json;
+                res.body.msg.should.equal('pong');
+                done()
+            }
+        });
+    })
+})
+
+describe('CRUD Post test', () => {
+    it('Should be return array of post when try to access "/"', (done) => {
+        chai.request('http://localhost:3000').get('/posts').end((err, res) => {
+            if (err) {
+                console.log(err);
+                done()
+            } else {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body[0].should.have.property('title');
+                res.body[0].should.have.property('body');
+                // res.body[0].should.have.property('slug');
+                done()
+            }
+        });
+    })
 })
