e78a6147f787ab2277e32b1d8b0645131021dea8
didietsuryadi <didietsuryadi>
2017-03-13 16:04:14 +0700

    create-routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var controller = require('../controllers/userController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('index', { title: 'Express' });
 });
+
+router.get('/login', controller.login)
 
 module.exports = router;
